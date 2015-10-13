a5d2be71c9b12906ae3f3f5ca5c8b4917e311fba
Chris Aporta <christopher.aporta@gmail.com>
2015-10-13 01:22:59 -0400

    complete lab

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
-# code here!
+require 'pry'
+
+class School
+
+  def initialize(school_name)
+    @school_name = school_name
+    @roster = {}
+  end
+
+  attr_accessor :school_name, :roster
+
+  def add_student(student_name, grade)
+    unless @roster[grade]
+      @roster[grade] = []
+    end
+    @roster[grade] << student_name
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each do |key, value|
+      @roster[key] = value.sort
+    end
+  end
+end
