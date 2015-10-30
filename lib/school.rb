4493aa00c0188ef3c208dc662beec8f21948d4fe
maiya777 <maiya777@list.ru>
2015-10-30 12:32:52 +0300

    Done

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,33 @@
-# code here!
+class School
+
+  attr_reader :roster
+
+  def initialize(school_name)
+    @school_name = school_name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    @roster[grade] = [] if !@roster.key?(grade)
+    @roster[grade] << name
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    sort = {}
+    @roster.each {|k, v| sort[k] = v.sort}
+    sort
+  end
+
+end
+      school = School.new("Test School")
+      school.add_student("Homer Simpson", 9)
+      school.add_student("Bart Simpson", 9)
+      school.add_student("Avi Flombaum", 10)
+      school.add_student("Jeff Baird", 10)
+      school.add_student("Blake Johnson", 7)
+      school.add_student("Jack Bauer", 7)
+      puts school.sort
