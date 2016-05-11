1e19494f49b5c88108f7781d4241263f87ae47f0
fhernandez173 <f.hernandez173@gmail.com>
2016-05-11 15:18:47 -0400

    done

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,37 @@
-# code here!
+require 'pry'
+
+# a domain model is a representation of real-world concepts in software.
+# classes can be templates for object creation
+# a class can rpoduce objects taht have attributes/ behaviors, enacted by instance methods
+
+class School 
+
+  attr_accessor :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(student_name, student_grade)
+    # binding.pry
+    @roster[student_grade] ||= []
+    @roster[student_grade] << student_name
+  end
+
+  def grade(class_grade)
+    @roster.each do |stud_grade, stud_name|
+      # binding.pry
+      if stud_grade == class_grade
+        return stud_name
+      end
+    end
+  end
+
+  def sort
+    @roster.each do |stud_grade, stud_name|
+      stud_name.sort!
+    end
+  end
+
+end
